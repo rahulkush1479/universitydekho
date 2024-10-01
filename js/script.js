c09d360653e1c7295bca8f6c2216b9d1dbfe2515
rahulkush1479 <165028502+rahulkush1479@users.noreply.github.com>
2024-10-01 10:53:41 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// function hideform() {
+//   var element = document.getElementById("hideform");
+//   element.style.display = "flex"
+// }
+
+
+$("showform").click(function(){
+    $("#hideform").toggleClass("block");
+  });
